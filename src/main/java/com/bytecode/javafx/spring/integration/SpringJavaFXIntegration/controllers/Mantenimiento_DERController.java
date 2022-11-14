58029a90a0b6f6dbfaf1c9fe8d01106045df1220
harmony365 <moisesramos@harmony365.es>
2022-11-14 06:13:30 +0100

    Se crearon las pantallas para darle mantenimiento a los DER, será una pantalla de tools.

---
+++ src/main/java/com/bytecode/javafx/spring/integration/SpringJavaFXIntegration/controllers/Mantenimiento_DERController.java
@@ -0,0 +1,50 @@
+package com.bytecode.javafx.spring.integration.SpringJavaFXIntegration.controllers;
+
+
+import com.bytecode.javafx.spring.integration.SpringJavaFXIntegration.App;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+@Component
+public class Mantenimiento_DERController implements Initializable {
+
+    @FXML
+    private Button btn_salir;
+
+    @FXML
+    private void eventAction(ActionEvent event) throws IOException {
+
+        Object evt = event.getSource();
+
+        if(evt.equals(btn_salir)){
+            Locale locale = Locale.getDefault();
+            App.setRoot("/views/primary",locale);
+        }
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // TODO Auto-generated method stub
+        if(App.parametrosModel.getAppDemo()){
+
+        }
+
+    }
+
+
+
+
+
+
+
+
+}
